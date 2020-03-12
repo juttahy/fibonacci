cbeb219bb7b905cf8385611c401fada0063213c3
Jutta Hyrskylahti <jutta.hyrskylahti@gmail.com>
2020-03-12 14:40:15 +0200

    added project to github

---
+++ app.js
@@ -0,0 +1,31 @@
+/* 
+Print Fibonacci numbers until the number is bigger than 1000
+*/
+
+const result = document.querySelector('#fibonacci-result');
+
+showFibonacciResults(1000);
+
+function showFibonacciResults(maxNumber) {
+  // Fibonaccin alkutilanne
+  const numArray = [1,1];
+
+  let lastNumInd;
+  let secLastNumInd;
+  let sum;
+  
+  for (let i = 1; numArray[i] < maxNumber; i++) {
+    lastNumInd = numArray.length - 1;
+    secLastNumInd = lastNumInd - 1;
+    sum = numArray[lastNumInd] + numArray[secLastNumInd];
+    numArray.push(sum);
+  }
+
+  printResults(numArray);
+
+}
+
+function printResults(array) {
+  const arrayList = array.join(', ');
+  result.textContent = arrayList;
+}
